81cca312fc2d7adc756b01d1ee005e45cd890a73
Kelly Jerrell <kcjerrell@gmail.com>
2021-06-04 13:03:52 -0700

    implemented useForm hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,20 @@
+import { useState } from "react";
+
 // write your custom hook here to control your checkout form
+const useForm = (emptyFormData) => {
+  const [formData, setFormData] = useState(emptyFormData);
+
+  const handleChange = e => {
+    // console.log(e)
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  const clearForm = e => {
+    setFormData(emptyFormData);
+  }
+
+  return [formData, handleChange, clearForm];
+}
+
+export default useForm;
